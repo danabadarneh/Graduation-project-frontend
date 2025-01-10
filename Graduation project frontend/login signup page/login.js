632baef1bb7de23241171f3caca946a9bc96bf89
Tasneem <tasneemgazal5@gmail.com>
2025-01-10 16:41:18 +0200

    Fix login issue

--- Graduation project frontend/login signup page/login.js
+++ Graduation project frontend/login signup page/login.js
@@ -5,7 +5,6 @@ document
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-
     if (!email || !password) {
       alert("Please fill in both fields.");
       return;
